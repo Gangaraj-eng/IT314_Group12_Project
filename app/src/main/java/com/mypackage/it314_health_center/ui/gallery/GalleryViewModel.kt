777276dc7297e8726a65632375be9d82a2290c3d
Akshar Chaudhari <202001071@daiict.ac.in>
2023-03-20 14:46:02 +0530

    Created Navigation Activity

---
+++ app/src/main/java/com/mypackage/it314_health_center/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.mypackage.it314_health_center.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
