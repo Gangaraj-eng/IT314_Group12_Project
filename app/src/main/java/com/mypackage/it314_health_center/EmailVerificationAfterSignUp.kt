7c24177ad37ab4a33948170e3ceb45267d5b00b9
Kavish <202001114@daiict.ac.in>
2023-03-21 16:25:19 +0530

    2 email verification pages added

---
+++ app/src/main/java/com/mypackage/it314_health_center/EmailVerificationAfterSignUp.kt
@@ -0,0 +1,11 @@
+package com.mypackage.it314_health_center
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EmailVerificationAfterSignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_email_verification_after_sign_up)
+    }
+}
